463dca369f15dbc47f304d9d7fe560852924fb8d
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-09-23 00:54:46 +0800

    update ch1 code

---
+++ CH1/hw1_13/hw1_13.c
@@ -0,0 +1,9 @@
+/* 印出"我愛語言" */
+
+#include<stdio.h>
+
+int main(){
+    printf("我愛語言\n");
+    return 0;
+
+}
---
+++ CH1/hw1_14/hw1_14.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void){
+    printf("我愛語言\n");
+    printf("這是我的第一個C語言程式\n");
+    return 0;
+
+
+}
