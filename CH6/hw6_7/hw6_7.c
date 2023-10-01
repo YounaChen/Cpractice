305feb8fb825c9a4a63732b825e14b44375a186f
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-10-01 13:32:57 +0800

    update ch6 code

--- CH6/hw6_7/hw6_7.c
+++ CH6/hw6_7/hw6_7.c
@@ -6,6 +6,7 @@ int main(void){
     int num2=5;
     int num3=5;
 
+
     if(num1+num2>num3 && num2+num3>num1 && num1+num3>num2)
         printf("%d %d %d可以構成三角形的三個邊長\n", num1, num2, num3);
     else
