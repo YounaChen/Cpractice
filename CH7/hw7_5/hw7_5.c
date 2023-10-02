e46ea05d3c287174fef9f12e38d6b2d3c0d83a33
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-10-02 17:22:21 +0800

    update ch7 code

--- CH7/hw7_5/hw7_5.c
+++ CH7/hw7_5/hw7_5.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main(void){
+
+    int i;
+    int sum = 0;
+    for(i=1;i<=100;i++){
+        if(i%3==0 && i%8==0){
+            sum = sum + i;
+        }
+    }
+
+    printf("同時可以被3和8整除的所有整數總和為:%d\n", sum);
+
+    system("pause");
+    return 0;
+
+}
