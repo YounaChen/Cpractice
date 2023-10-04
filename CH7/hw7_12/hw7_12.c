57f34929a5d1a7b754b6c58e3ea527cf4c089e7c
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-10-04 16:40:14 +0800

    update ch7 code

--- CH7/hw7_12/hw7_12.c
+++ CH7/hw7_12/hw7_12.c
@@ -5,12 +5,23 @@
 
 int main(void){
 
-    /*endless loop*/
-    int i=1;
-
-    while(i>0){
-        printf("i=%d\n", i++);
+    /*Armstrong*/
+    int sum=0;
+    int temp,n1,n2,n3;
+    printf("所有的Armstrong數:\n");
+    int i;
+    for(i=100;i<1000;i++){
+        n3=i%10;
+        temp=i/10;
+        n2=temp%10;
+        n1=temp/10;
+        sum=pow(n1,3)+pow(n2,3)+pow(n3,3);
+        if(i==sum){
+            printf("%d ", i);
+        }
     }
+
+    printf("\n");
 
     system("pause");
     return 0;
