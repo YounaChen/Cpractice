f3dec42cfbe9b06089cb2d026c5d095b285cb66e
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-10-12 01:55:59 +0800

    update ch8 code

--- CH8/prog8_28/prog8_28.c
+++ CH8/prog8_28/prog8_28.c
@@ -1,24 +1,16 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 
-void add10(int,int);
+
 
 int main(void){
-    int a=3, b=5;
 
-    printf("呼叫函數add10()之前: ");
-    printf("a=%d, b=%d\n", a, b);
-    add10(a,b);
-    printf("呼叫函數add10()之後: ");
-    printf("a=%d, b=%d\n", a, b);
+
+
     system("pause");
     return 0;
 
-}
-
-void add10(int a, int b){
-    a=a+10;
-    b=b+10;
 }
 
 
