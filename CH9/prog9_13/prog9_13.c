c8fb8713970d47fd886dd8b7dc9a0caab3c5cd20
YounaChen <145748914+YounaChen@users.noreply.github.com>
2023-10-14 21:26:26 +0800

    update ch9 code

--- CH9/prog9_13/prog9_13.c
+++ CH9/prog9_13/prog9_13.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void func(int);
+
 int main(){
 
-    printf("你好\n");
+    int a=13;
+    printf("於main()裡，a=%d ,a的位址=%p\n", a, &a);
+    func(a);
 
+    system("pause");
     return 0;
+}
+
+void func(int a){
+    printf("於func()裡，a=%d, a的位址=%p\n", a, &a);
 }
--- CH9/prog9_14/prog9_14.c
+++ CH9/prog9_14/prog9_14.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define SIZE 3
+void func(int arr[]);
+
+int main(){
+
+    int i, A[SIZE]={20,8,13};
+    printf("在main()裡，陣列A元素的位址為\n");
+    for(i=0;i<SIZE;i++){
+        printf("A[%d]=%2d,位址為%p\n", i, A[i], &A[i]);
+    }
+    func(A);
+
+    system("pause");
+    return 0;
+}
+
+void func(int arr[]){
+    int i;
+    printf("\n在func()裡，陣列arr元素的位址為\n");
+    for(i=0;i<SIZE;i++){
+        printf("arr[%d]=%2d,位址為%p\n", i, arr[i], &arr[i]);
+    }
+    
+}
 
